17376c5b5a9c4838be28795035555cde8df52b5c
Mayur Soni <mayurssoni@hotmail.com>
2020-08-09 14:05:25 +0000

    made some changes to to clear sonar code smells

--- src/main/java/com/casestudy/spring/GreetingController.java
+++ src/main/java/com/casestudy/spring/GreetingController.java
@@ -5,12 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GreetingController {
 
-//Uage /greeting?name=Mayur
+//Usage /greeting?name=Mayur
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
@@ -20,7 +19,8 @@ public class GreetingController {
 //usage /countcharacters?word=String
         @GetMapping("/countcharacters")
         public String countChar(@RequestParam(name="word",required=false, defaultValue="World") String word, Model model) {
-		int vcount = 0, ccount = 0;
+		int vcount = 0;
+		int ccount = 0;
 		word=word.toLowerCase();
 		for(int i = 0; i < word.length(); i++) { 
 			char ch = word.charAt(i); 
